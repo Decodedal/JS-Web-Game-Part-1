43d48a666f2a984179b9a0b6c65b72ea00f562f6
Dallas Palumbo <palumbodallas@gmail.com>
2022-06-14 22:45:45 -0400

    halfway

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newimg(url,left,bottom){
+let obj = document.createElement('img')
+obj.src = url
+obj.style.position = 'fixed'
+obj.style.left = left + "px"
+obj.style.bottom = bottom +'px'
+document.body.append(obj)
+return obj
+}
+
+newimg('assets/green-character.gif',"100","100")
+newimg("assets/pine-tree.png","450","200")
+newimg("assets/sheild.png","165","185")
+newimg("assets/staff.png","600","100")
+
+
+
+
+
+
+// let pineTree =document.createElement("img")
+// pineTree.src = "assets/pine-tree.png"
+// pineTree.style.position = "fixed"
+// pineTree.style.left = "450px"
+// pineTree.style.bottom = "200px"
+// document.body.append(pineTree)
