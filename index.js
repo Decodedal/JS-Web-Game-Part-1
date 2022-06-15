9c1f6a29a5cb0ad463c295951c91f6c07c6f7187
Dallas Palumbo <palumbodallas@gmail.com>
2022-06-14 23:08:15 -0400

    finished

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// function creates new images
 function newimg(url,left,bottom){
 let obj = document.createElement('img')
 obj.src = url
@@ -8,12 +9,26 @@ obj.style.bottom = bottom +'px'
 document.body.append(obj)
 return obj
 }
-
+// adding new images
 newimg('assets/green-character.gif',"100","100")
 newimg("assets/pine-tree.png","450","200")
-newimg("assets/sheild.png","165","185")
-newimg("assets/staff.png","600","100")
+newimg("assets/tree.png","200","300")
+newimg("assets/pillar.png","350","100")
+newimg("assets/crate.png","150","200")
+newimg("assets/well.png","500","425")
 
+// a function creates new items that dissapear on double click
+function pickUp(url,left,bottom){
+    let sword = newimg(url,left,bottom)
+
+    sword.addEventListener("dblclick", function(){
+         sword.remove()
+    })
+}
+// objects ti be picked up 
+pickUp("assets/sword.png","500","405")
+pickUp("assets/sheild.png","165","185")
+pickUp("assets/staff.png","600","100")
 
 
 
